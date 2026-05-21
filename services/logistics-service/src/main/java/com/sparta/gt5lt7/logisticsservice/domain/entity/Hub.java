250ea6efc0ba8d08526330b6de19f0c1900551df
181108-cherry <dysy9799@naver.com>
2026-05-21 15:39:50 +0900

    feat(hub): 허브 엔티티 생성

--- services/logistics-service/src/main/java/com/sparta/gt5lt7/logisticsservice/domain/entity/Hub.java
+++ services/logistics-service/src/main/java/com/sparta/gt5lt7/logisticsservice/domain/entity/Hub.java
@@ -1,4 +1,60 @@
 package com.sparta.gt5lt7.logisticsservice.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_hubs")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Hub {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "hub_id")
+    private UUID hubId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    // 위도
+    @Column(name = "latitude", nullable = false)
+    private Double latitude;
+
+    // 경도
+    @Column(name = "longitude", nullable = false)
+    private Double longitude;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by")
+    private String deletedBy;
+
+
+
 }
