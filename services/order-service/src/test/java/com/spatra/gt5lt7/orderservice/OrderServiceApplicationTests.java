dd61f594fdf34ee554e4c61e46e0af6384af7e78
riiach <riiachoii@gmail.com>
2026-05-22 15:50:54 +0900

    chore: order-service 초기 환경 세팅

---
+++ services/order-service/src/test/java/com/spatra/gt5lt7/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spatra.gt5lt7.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
