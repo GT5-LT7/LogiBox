14814cd99f659a3da5096d6c33c24cd0da7f00bc
gray-ji <seeu314@gmail.com>
2026-05-23 13:25:24 +0900

    fix(catalog): searchProducts() 내 fetchJoin 오류 수정

--- services/catalog-service/src/main/java/com/sparta/gt5lt7/catalog/domain/repository/ProductRepositoryImpl.java
+++ services/catalog-service/src/main/java/com/sparta/gt5lt7/catalog/domain/repository/ProductRepositoryImpl.java
@@ -4,6 +4,8 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.gt5lt7.catalog.domain.entity.Product;
+import com.sparta.gt5lt7.catalog.domain.entity.QCategory;
+import com.sparta.gt5lt7.catalog.domain.entity.QCompany;
 import com.sparta.gt5lt7.catalog.domain.entity.QProduct;
 import com.sparta.gt5lt7.catalog.global.util.QueryDslUtil;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +25,11 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
     @Override
     public Page<Product> searchProducts(String keyword, UUID companyId, UUID hubId, UUID categoryId, Pageable pageable) {
         QProduct product = QProduct.product;
+
+        // 조인용 Q클래스
+        QCompany company = QCompany.company;
+        QCategory category = QCategory.category;
+
         BooleanBuilder builder = new BooleanBuilder();
 
         // 1. 검색 및 필터 조건
@@ -30,13 +37,13 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
             builder.and(product.name.containsIgnoreCase(keyword));
         }
         if (companyId != null) {
-            builder.and(product.company.companyId.eq(companyId));
+            builder.and(company.companyId.eq(companyId));
         }
         if (hubId != null) {
-            builder.and(product.company.hubId.eq(hubId));
+            builder.and(company.hubId.eq(hubId));
         }
         if (categoryId != null) {
-            builder.and(product.category.categoryId.eq(categoryId));
+            builder.and(category.categoryId.eq(categoryId));
         }
 
         // 2. 정렬 조건
@@ -45,16 +52,20 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
         // 3. 실제 데이터 조회
         List<Product> content = queryFactory
                 .selectFrom(product)
+                .leftJoin(product.company, company).fetchJoin()
+                .leftJoin(product.category, category).fetchJoin()
                 .where(builder)
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .orderBy(orderSpecifiers.toArray(new OrderSpecifier[0]))
-                .fetchJoin().fetch();
+                .fetch();
 
         // 4. total 조회
         Long total = queryFactory
                 .select(product.count())
                 .from(product)
+                .leftJoin(product.company, company)
+                .leftJoin(product.category, category)
                 .where(builder)
                 .fetchOne();
         long totalCount = (total != null) ? total : 0L;
