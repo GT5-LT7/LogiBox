c7b5c39ffff620959af2ac71e048d78a1b5891b5
181108-cherry <dysy9799@naver.com>
2026-05-24 16:24:25 +0900

    refactor: 공통 모듈 SecurityConfig 사용으로 변경
    - develop 머지로 공통 모듈에 표준 SecurityConfig가 추가됨에 따라 logistics-service의 로컬 SecurityConfig를 삭제하여 필터 체인 중복을 제거
    - 함께 도입했던 임시 우회 설정(ManagementWebSecurityAutoConfiguration exclude, autoconfigure.exclude property)도 더 이상 불필요하므로 정리

--- services/logistics-service/src/main/java/com/sparta/gt5lt7/logisticsservice/LogisticsServiceApplication.java
+++ services/logistics-service/src/main/java/com/sparta/gt5lt7/logisticsservice/LogisticsServiceApplication.java
@@ -1,12 +1,10 @@
 package com.sparta.gt5lt7.logisticsservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = { ManagementWebSecurityAutoConfiguration.class })
+@SpringBootApplication
 public class LogisticsServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LogisticsServiceApplication.class, args);
     }
