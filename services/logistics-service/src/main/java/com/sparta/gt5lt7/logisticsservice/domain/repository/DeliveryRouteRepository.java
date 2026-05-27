1b90e5040f5d1320d8640ec97c77adffc058074a
riiach <riiachoii@gmail.com>
2026-05-27 12:18:06 +0900

    feat: 배송 삭제를 위한 배송 경로 레포 수정

--- services/logistics-service/src/main/java/com/sparta/gt5lt7/logisticsservice/domain/repository/DeliveryRouteRepository.java
+++ services/logistics-service/src/main/java/com/sparta/gt5lt7/logisticsservice/domain/repository/DeliveryRouteRepository.java
@@ -4,9 +4,12 @@ import com.sparta.gt5lt7.logisticsservice.domain.entity.DeliveryRoute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DeliveryRouteRepository extends JpaRepository<DeliveryRoute, UUID> {
+
+    Optional<DeliveryRoute> findByIdAndDeletedAtIsNull(UUID routeId);
 
     List<DeliveryRoute> findAllByDeliveryIdAndDeletedAtIsNullOrderBySequenceAsc(UUID deliveryId);
 }
