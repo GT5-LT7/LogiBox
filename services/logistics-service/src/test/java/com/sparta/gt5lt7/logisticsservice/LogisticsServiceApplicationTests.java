c7d7464ef8dab65904d89e12740971611ff83abd
riiach <riiachoii@gmail.com>
2026-05-27 13:40:45 +0900

    chore: 테트스용 어플리케이션 파일에 더미 프로퍼티 추가

--- services/logistics-service/src/test/java/com/sparta/gt5lt7/logisticsservice/LogisticsServiceApplicationTests.java
+++ services/logistics-service/src/test/java/com/sparta/gt5lt7/logisticsservice/LogisticsServiceApplicationTests.java
@@ -2,8 +2,14 @@ package com.sparta.gt5lt7.logisticsservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(properties = {
+        "kakao.local.api-key=test-kakao-api-key",
+        "redis.host=localhost",
+        "eureka.client.enabled=false"
+})
 class LogisticsServiceApplicationTests {
 
     @Test
